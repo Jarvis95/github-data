1663945514d43b3521e7704cbdedd94b3db0e644
Akshay Deogade <akshay@Akshays-MacBook-Air.local>
2025-09-29 00:55:58 +0530

    modify main

--- src/main/java/com/example/github/GithubDataApplication.java
+++ src/main/java/com/example/github/GithubDataApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GithubDataApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(GithubDataApplication.class, args);
         System.out.println("Hello World :: Github Data Application");
